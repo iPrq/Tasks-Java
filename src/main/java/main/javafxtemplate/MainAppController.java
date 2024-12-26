2ff2eed0fc57c772673f065702732b0aa4dd2d3c
iPrq <danciju@gmail.com>
2024-12-26 16:05:59 +0530

    Added Functionality to Vbox

--- src/main/java/main/javafxtemplate/MainAppController.java
+++ src/main/java/main/javafxtemplate/MainAppController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainAppController {
@@ -11,7 +12,8 @@ public class MainAppController {
     @FXML
     private Pane titlePane;
     private double x,y;
-
+    @FXML
+    private VBox tasksVbox;
 
     public void init(Stage stage) {
         titlePane.setOnMousePressed(MouseEvent -> {
@@ -22,6 +24,15 @@ public class MainAppController {
             stage.setX(MouseEvent.getScreenX() - x);
             stage.setY(MouseEvent.getScreenY() - y);
         });
+    }
+
+    @FXML
+    private void newtask() {
+        Pane taskPane = new Pane();
+        taskPane.setStyle("-fx-background-color: #00000026;");
+        taskPane.setMinSize(537,49);
+        taskPane.setMaxSize(537,49);
+        tasksVbox.getChildren().add(taskPane);
     }
 
 }
