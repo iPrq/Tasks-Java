7edb57d06ea09758a46965f3c9c6fb47fcfee72a
Neeraj Ciju <danciju@gmail.com>
2024-12-26 15:45:51 +0530

    Added Functionality to titlePane

--- src/main/java/main/javafxtemplate/MainAppController.java
+++ src/main/java/main/javafxtemplate/MainAppController.java
@@ -2,7 +2,26 @@ package main.javafxtemplate;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class MainAppController {
+
+    @FXML
+    private Pane titlePane;
+    private double x,y;
+
+
+    public void init(Stage stage) {
+        titlePane.setOnMousePressed(MouseEvent -> {
+            x = MouseEvent.getSceneX();
+            y = MouseEvent.getSceneY();
+        });
+        titlePane.setOnMouseDragged(MouseEvent -> {
+            stage.setX(MouseEvent.getScreenX() - x);
+            stage.setY(MouseEvent.getScreenY() - y);
+        });
+    }
 
 }
--- src/main/java/main/javafxtemplate/MainApplication.java
+++ src/main/java/main/javafxtemplate/MainApplication.java
@@ -3,18 +3,23 @@ package main.javafxtemplate;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Template-fxml.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("TasksAppGUI.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Template");
+        stage.initStyle(StageStyle.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setTitle("Tasks");
         stage.setScene(scene);
         stage.show();
+        ((MainAppController)fxmlLoader.getController()).init(stage);
     }
 
     public static void run() {
